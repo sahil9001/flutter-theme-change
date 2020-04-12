d44bd589383947c615b1f206482cc024fd857307
sahil9001 <sahilsilare@gmail.com>
2020-04-12 12:16:29 +0530

    Changes to be committed: 	modified:   lib/pages/home.dart

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -8,27 +8,78 @@ class HomePage extends StatelessWidget {
     ThemeChanger _themeChanger = Provider.of<ThemeChanger>(context);
 
     return new Scaffold(
-      appBar: AppBar(
-        title: Text('COVID'),
-        centerTitle: true,
-        
-      ),
-      body: Center(
-        child: Text('Welcome to my app!',
-        style: TextStyle(
-          fontSize: 40.0,
-          fontFamily: 'Roboto' 
-        ),),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () => {
-          if (_themeChanger.getTheme() == ThemeData.dark())
-            _themeChanger.setTheme(ThemeData.light())
-          else
-            _themeChanger.setTheme(ThemeData.dark())
-        },
-        child: Icon(Icons.brightness_6),
-      ),
-    );
+        appBar: AppBar(
+          title: Text('My app'),
+          actions: <Widget>[
+            IconButton(
+                icon: Icon(Icons.whatshot),
+                onPressed: () => {
+                      if (_themeChanger.getTheme() == ThemeData.dark())
+                        _themeChanger.setTheme(ThemeData.light())
+                      else
+                        _themeChanger.setTheme(ThemeData.dark())
+                    }),
+          ],
+        ),
+        body: Padding(
+          padding: EdgeInsets.fromLTRB(30.0, 40.0, 30.0, 0.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Center(
+                child: CircleAvatar(
+                  backgroundImage: NetworkImage('https://cdn.fastly.picmonkey.com/contentful/h6goo9gw1hh6/2sNZtFAWOdP1lmQ33VwRN3/24e953b920a9cd0ff2e1d587742a2472/1-intro-photo-final.jpg?w=800&q=70'),
+                  radius: 40.0,
+                  
+                ),
+              ),
+              SizedBox(height:30.0),
+              Text(
+                'NAME',
+                style: TextStyle(color: Colors.grey, letterSpacing: 2.0),
+              ),
+              SizedBox(height: 10.0),
+              Text(
+                'John Jefferson',
+                style: TextStyle(
+                    color: Colors.amberAccent[200],
+                    letterSpacing: 2.0,
+                    fontSize: 28.0,
+                    fontWeight: FontWeight.bold),
+              ),
+              SizedBox(height: 30.0),
+              Text(
+                'CURRENT NINJA LEVEL',
+                style: TextStyle(
+                    color: Colors.grey,
+                    letterSpacing: 2.0,
+                    fontWeight: FontWeight.bold),
+              ),
+              SizedBox(height: 10.0),
+              Text(
+                '8',
+                style: TextStyle(
+                    color: Colors.amberAccent[200],
+                    letterSpacing: 2.0,
+                    fontSize: 28.0,
+                    fontWeight: FontWeight.bold),
+              ),
+              SizedBox(height: 30.0),
+              Row(
+                children: <Widget>[
+                  Icon(
+                    Icons.email,
+                    color: Colors.grey[400],
+                  ),
+                  SizedBox(width:10.0),
+                  Text(
+                    'johnjeff@gmail.com',
+                    style: TextStyle(color: Colors.grey[400], fontSize: 18.0, letterSpacing: 1.0),
+                  )
+                ],
+              )
+            ],
+          ),
+        ));
   }
 }
